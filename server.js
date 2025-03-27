ed9755a213e9299e5c76265ddd0afae4cd96cd77
Vandanx712 <vadanboghara@gmail.com>
2025-03-27 23:38:22 +0530

    commment out in server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
-app.use(cookieparser())
+// app.use(cookieparser())
 
 app.use('/api',indexRoute)
 
