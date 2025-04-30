f8283114c37e8658298811645c5dcb45092d677e
Vandanx712 <vadanboghara@gmail.com>
2025-04-30 18:56:24 +0530

    apply rate-limit logic in server.js file

--- server.js
+++ server.js
@@ -5,15 +5,22 @@ import indexRoute from './routes/index.route.js'
 import { connectDB } from './db/db.js'
 import { handleError } from './utill/apierror.js'
 import defaultData from './db/default.js'
+import rateLimit from 'express-rate-limit'
 
 
 dotenv.config()
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 50, 
+    message: "Too many requests, please try again later.",
+});
 const port = process.env.PORT
 
 await connectDB()
 await defaultData()
 const app = express()
 
+app.use(limiter)
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
